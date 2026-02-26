dc02f3a08669107d42b1bed93b197b5698bd5729
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2026-02-25 19:12:03 -0500

    Adds Wayland support and adjusts default times

--- discord_anti_idle.sh
+++ discord_anti_idle.sh
@@ -3,25 +3,34 @@ set -euo pipefail
 
 # Configure appropriately depending on the AFK timer of your Discord server.
 # The amount of time after which the command is triggered
-IDLE_TIME_MINUTES=5
+IDLE_TIME_MINUTES=15
 # Time between idle checks
-SLEEP_TIME_MINUTES=1
+SLEEP_TIME_MINUTES=5
 
 # The default (Ctrl+/) opens and closes the keyboard shortcuts window.
 # Should be fairly non-intrusive but other shortcuts could be used here, such
 # as keying up and releasing PTT, if the behavior is undesirable.
 ANTI_IDLE_KEYS="ctrl+slash"
 
+# Location of the empty file that will be created whenever the user is idle
+# on a system using Wayland.
+WAYLAND_IDLE_FILE="${HOME}/isidle"
+
 # Script dependencies, do not modify.
 REQUIRED_TOOLS=(
     "xdotool"
-    "xprintidle"
     "wmctrl"
 )
 
 check_for_dependencies() {
     local valid="true"
     local missing=()
+
+    if is_wayland; then
+        REQUIRED_TOOLS+=("swayidle")
+    else
+        REQUIRED_TOOLS+=("xprintidle")
+    fi
 
     for utility in "${REQUIRED_TOOLS[@]}"; do
         if ! command -v $utility &>/dev/null; then
@@ -46,6 +55,16 @@ is_screen_locked() {
   else
     return 1
   fi
+}
+
+is_wayland() {
+    if [[ -v "WAYLAND_DISPLAY" && -n $WAYLAND_DISPLAY ]] ||
+       [[ $XDG_SESSION_TYPE == "wayland" ]] ||
+       [[ $DESKTOP_SESSION =~ .*wayland ]]
+    then
+        return 0
+    fi
+    return 1
 }
 
 launch_discord() {
@@ -78,27 +97,39 @@ trigger_anti_idle_cmd() {
 
     # When launched from the desktop shortcut, this will log to the journal.
     # View with `journalctl -b` to validate it is working properly.
-    echo "Triggered Discord anti-idle command sucessfully."
+    echo "Triggered Discord anti-idle command successfully."
 }
 
-monitor_x_for_idle() {
-    while sleep $SLEEP_TIME; do
+monitor_session_for_idle() {
+    if is_wayland; then
+        swayidle -w timeout $IDLE_TIME_SEC "touch ${WAYLAND_IDLE_FILE}" resume "rm ${WAYLAND_IDLE_FILE}" &
+    fi
+
+    while sleep $SLEEP_TIME_SEC; do
         if ! wmctrl -l | grep -q Discord &>/dev/null; then
+            if is_wayland; then
+                pkill swayidle
+            fi
             # Discord exited, exit script
             exit 0
         fi
         if ! is_screen_locked; then
-            idle=$(xprintidle)
-            if [[ $idle -ge $IDLE_TIME ]]; then
-                trigger_anti_idle_cmd
+            if is_wayland; then
+                [[ -f $WAYLAND_IDLE_FILE ]] && trigger_anti_idle_cmd
+            else
+                idle=$(xprintidle)
+                if [[ $idle -ge $IDLE_TIME_MS ]]; then
+                    trigger_anti_idle_cmd
+                fi
             fi
         fi
     done
 }
 
-IDLE_TIME=$((IDLE_TIME_MINUTES*60*1000))
-SLEEP_TIME=$((SLEEP_TIME_MINUTES*60))
+IDLE_TIME_SEC=$((IDLE_TIME_MINUTES*60))
+IDLE_TIME_MS=$((IDLE_TIME_SEC*1000))
+SLEEP_TIME_SEC=$((SLEEP_TIME_MINUTES*60))
 
 check_for_dependencies
 launch_discord
-monitor_x_for_idle
+monitor_session_for_idle
